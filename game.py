e69232970ba0b368385da6944758d0a54445c624
zacrichins <zacrichins12@gmail.com>
2023-02-10 11:18:23 -0700

    starting

---
+++ game.py
@@ -0,0 +1,14 @@
+#room details
+maincontrolroom = ("Main control room: Large crescent shaped room where the whole power plant is directed from")
+hallway = ("")
+
+
+
+#introduction
+print("You are Vasily Ignatenko, a rookie nuclear physicist. The date is April 26 1986 and you have just been promoted to work in the Chernobyl main control room.")
+print("Tonight you are being directed to run a mandatory energy test. Even though the preparations are not made, the Commander demands the test to be run.")
+print("The test failed. Disaster strikes. Chernobyl has exploded.")
+print("Escape")
+
+print()
+print(maincontrolroom)
